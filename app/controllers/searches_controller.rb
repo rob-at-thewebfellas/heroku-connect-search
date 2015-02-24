6868ab0e37e3c765fc00c984fa89030693533763
Rob Anderton <rob.anderton@thewebfellas.com>
2015-02-24 16:30:27 +0000

    add a class filter to global search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,10 +1,17 @@
 class SearchesController < ApplicationController
 
+  SEARCH_CLASSES = [
+    Company,
+    Salesforce::OrganisationA::Contact,
+    Salesforce::OrganisationB::Contact
+  ].freeze
+
   include SearchableController
 
   def index
     @resources = ThinkingSphinx.search(
       ThinkingSphinx::Query.escape(search_params[:keywords].to_s),
+      classes: search_params[:class_names],
       order: :name,
       page: search_params[:page],
       per_page: Kaminari.config.default_per_page,
@@ -13,5 +20,14 @@ class SearchesController < ApplicationController
 
     respond_with(@resources)
   end
+
+  protected
+
+    def initialize_search_params
+      super.tap do |s|
+        s[:classes] = Array.wrap(s[:classes] || [0, 1, 2]).map(&:to_i)
+        s[:class_names] = s[:classes].map { |type| SEARCH_CLASSES[type] }.delete_if(&:blank?)
+      end
+    end
 
 end
